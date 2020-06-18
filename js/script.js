22e1f43a87051ca7a67fc5655f1d16086fb9b2d4
AReish <62810514+AReish@users.noreply.github.com>
2020-06-18 17:06:16 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+	$('.sidebar__menu-burger').click(function () {
+		$('.sidebar-menu, .home, .sidebar__menu-text, .sidebar__menu-burger, .sidebar__text').toggleClass('active')
+	});
+});
